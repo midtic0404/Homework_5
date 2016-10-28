4fc1f36bbc784fe903ea2edbee67ee87cf683c57
Chia Che Chang <midtic0404@gmail.com>
2016-10-28 00:22:22 +0000

    Adding GetInput function

--- Chia_Chang_Hw5.py
+++ Chia_Chang_Hw5.py
@@ -2,6 +2,29 @@
 import sys
 
 
+pin = "1234"
+
+def GetInput():
+    print("Enter your pin: ")
+    uPin = input()
+
+    if(len(uPin) != 4):
+        print("Wrong Length")
+    else:
+        print("Good format")
+    
+    return uPin
+
+
+
+count = 0
+while (count < 3):
+    print("Hi")
+    count  += 1
+
+
+
+
 
 
 def main():
